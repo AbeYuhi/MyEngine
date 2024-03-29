eb79e7bd772bdea814745cc45fe3db3b0bf275e9
AbeYuhi <k022g0032@g.neec.ac.jp>
2024-03-29 12:30:27 +0900

    a

--- MyEngine/DirectXGame/Manager/GraphicsPipelineManager.cpp
+++ MyEngine/DirectXGame/Manager/GraphicsPipelineManager.cpp
@@ -171,22 +171,52 @@ void GraphicsPipelineManager::CreatePSO() {
 	DirectXCommon* directXCommon = DirectXCommon::GetInstance();
 
 	//InputLayoutの設定
-	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[3] = {};
-	inputElementDescs[0].SemanticName = "POSITION";
-	inputElementDescs[0].SemanticIndex = 0;
-	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-	inputElementDescs[1].SemanticName = "TEXCOORD";
-	inputElementDescs[1].SemanticIndex = 0;
-	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
-	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-	inputElementDescs[2].SemanticName = "NORMAL";
-	inputElementDescs[2].SemanticIndex = 0;
-	inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
-	inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-	inputLayoutDesc.pInputElementDescs = inputElementDescs;
-	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc[PipelineState::kCountOfPipelineState] = {};
+	for (uint32_t shaderPack = 0; shaderPack < PipelineState::kCountOfPipelineState; shaderPack++) {
+		switch (shaderPack)
+		{
+		case PipelineState::kDefault:
+		case PipelineState::kWireFrame:
+		default:
+		{
+			D3D12_INPUT_ELEMENT_DESC inputElementDescs[3] = {};
+			inputElementDescs[0].SemanticName = "POSITION";
+			inputElementDescs[0].SemanticIndex = 0;
+			inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+			inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+			inputElementDescs[1].SemanticName = "TEXCOORD";
+			inputElementDescs[1].SemanticIndex = 0;
+			inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+			inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+			inputElementDescs[2].SemanticName = "NORMAL";
+			inputElementDescs[2].SemanticIndex = 0;
+			inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+			inputLayoutDesc[shaderPack].pInputElementDescs = inputElementDescs;
+			inputLayoutDesc[shaderPack].NumElements = _countof(inputElementDescs);
+		}
+			break;
+		case PipelineState::kParticle:
+		{
+			D3D12_INPUT_ELEMENT_DESC inputElementDescs[3] = {};
+			inputElementDescs[0].SemanticName = "POSITION";
+			inputElementDescs[0].SemanticIndex = 0;
+			inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+			inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+			inputElementDescs[1].SemanticName = "TEXCOORD";
+			inputElementDescs[1].SemanticIndex = 0;
+			inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+			inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+			inputElementDescs[2].SemanticName = "NORMAL";
+			inputElementDescs[2].SemanticIndex = 0;
+			inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+			inputLayoutDesc[shaderPack].pInputElementDescs = inputElementDescs;
+			inputLayoutDesc[shaderPack].NumElements = _countof(inputElementDescs);
+		}
+			break;
+		}
+	}
 
 	//BlendStateの設定
 	D3D12_BLEND_DESC blendDesc[BlendMode::kCountOfBlendMode] = {0};
@@ -309,7 +339,7 @@ void GraphicsPipelineManager::CreatePSO() {
 	for (int shaderPack = 0; shaderPack < PipelineState::kCountOfPipelineState; shaderPack++) {
 		for (int blendMode = 0; blendMode < BlendMode::kCountOfBlendMode; blendMode++) {
 			graphicsPipeLineStateDesc.pRootSignature = rootSignature_[shaderPack].Get();
-			graphicsPipeLineStateDesc.InputLayout = inputLayoutDesc;
+			graphicsPipeLineStateDesc.InputLayout = inputLayoutDesc[shaderPack];
 			graphicsPipeLineStateDesc.VS = { vertexShaderBlob[shaderPack]->GetBufferPointer(), vertexShaderBlob[shaderPack]->GetBufferSize() };
 			graphicsPipeLineStateDesc.PS = { pixelShaderBlob[shaderPack]->GetBufferPointer(), pixelShaderBlob[shaderPack]->GetBufferSize() };
 			graphicsPipeLineStateDesc.BlendState = blendDesc[blendMode];
