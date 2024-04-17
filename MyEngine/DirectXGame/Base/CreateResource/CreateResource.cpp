2bf2e386ae7243cfa070cb5f661842c0d123c32c
AbeYuhi <k022g0032@g.neec.ac.jp>
2024-04-17 16:57:11 +0900

    途中

--- MyEngine/DirectXGame/Base/CreateResource/CreateResource.cpp
+++ MyEngine/DirectXGame/Base/CreateResource/CreateResource.cpp
@@ -20,3 +20,24 @@ ComPtr<ID3D12Resource> CreateBufferResource(size_t sizeInBytes) {
 	assert(SUCCEEDED(hr));
 	return vertexResource;
 }
+
+ComPtr<ID3D12Resource> CreateRenderTextureResoruce(uint32_t width, uint32_t height, DXGI_FORMAT format, const Vector4& clearColor) {
+	//頂点リソース用のヒープの設定
+	D3D12_HEAP_PROPERTIES heapProperties{};
+	heapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
+	//頂点リソースの設定
+	D3D12_RESOURCE_DESC resourceDesc{};
+	//バックバッファリソース
+	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resourceDesc.Height = 1;
+	resourceDesc.DepthOrArraySize = 1;
+	resourceDesc.MipLevels = 1;
+	resourceDesc.SampleDesc.Count = 1;
+	resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
+	resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	//実際に生成
+	ComPtr<ID3D12Resource> vertexResource;
+	LRESULT hr = DirectXCommon::GetInstance()->GetDevice()->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
+	assert(SUCCEEDED(hr));
+	return vertexResource;
+}
