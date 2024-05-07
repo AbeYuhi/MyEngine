17e8c67dcbe0ca206435acc688a1605386867065
友飛 阿部 <maronn.0106.2525@gmail.com>
2024-05-07 20:16:27 +0900

    オブジェクトをグレイスケールできるように

--- MyEngine/Resources/Shaders/Object3d.PS.hlsl
+++ MyEngine/Resources/Shaders/Object3d.PS.hlsl
@@ -226,5 +226,7 @@ PixelShaderOutput main(VertexShaderOutput input)
         discard;
     }
     
+    float32_t value = dot(output.color.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
+    output.color.rgb = float32_t3(value, value, value);
     return output;
 }
